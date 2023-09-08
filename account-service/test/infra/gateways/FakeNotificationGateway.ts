3677b2a0914da14c2325824ddba225402dbfad28
mrdiniz88 <mr.diniz8888@gmail.com>
2023-09-07 21:33:32 -0300

    feat(account-service): remove FakeNotificationGateway

--- account-service/test/infra/gateways/FakeNotificationGateway.ts
+++
@@ -1,7 +0,0 @@
-import NotificationGateway from "../../../src/application/gateways/NotificatonGateway";
-
-export default class FakeNotificationGateway implements NotificationGateway {
-  async sendEmail(email: string, body: string): Promise<void> {
-    return;
-  }
-}
