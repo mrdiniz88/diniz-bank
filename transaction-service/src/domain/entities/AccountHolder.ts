37d83117c93cafc7a7ca6aea757bd83c1d441e39
mrdiniz88 <mr.diniz8888@gmail.com>
2023-09-08 21:54:34 -0300

    feat(transaction-service): add entities
    account holder
    transaction

---
+++ transaction-service/src/domain/entities/AccountHolder.ts
@@ -0,0 +1,3 @@
+export default class AccountHolder {
+  constructor(readonly id: string, readonly name: string) {}
+}
--- transaction-service/src/domain/entities/Transaction.ts
+++ transaction-service/src/domain/entities/Transaction.ts
@@ -1,14 +1,56 @@
 import { randomUUID } from "crypto";
+import AccountHolder from "./AccountHolder";
+
+type Status = "PROCESSING" | "APPROVED" | "DENIED" | "ERROR";
 
 export default class Transaction {
   constructor(
     readonly id: string,
-    readonly senderId: string,
-    readonly receiverId: string,
-    readonly amount: number
+    readonly sender: AccountHolder,
+    readonly receiver: AccountHolder,
+    readonly amount: number,
+    private status: Status,
+    readonly createdAt: Date,
+    private updatedAt: Date
   ) {}
 
-  static create(senderId: string, receiverId: string, amount: number) {
-    return new Transaction(randomUUID(), senderId, receiverId, amount);
+  static create(
+    sender: AccountHolder,
+    receiver: AccountHolder,
+    amount: number
+  ) {
+    const currentDate = new Date();
+    return new Transaction(
+      randomUUID(),
+      sender,
+      receiver,
+      amount,
+      "PROCESSING",
+      currentDate,
+      currentDate
+    );
+  }
+
+  denied() {
+    this.status = "DENIED";
+    this.updatedAt = new Date();
+  }
+
+  approved() {
+    this.status = "APPROVED";
+    this.updatedAt = new Date();
+  }
+
+  error() {
+    this.status = "ERROR";
+    this.updatedAt = new Date();
+  }
+
+  getStatus(): Status {
+    return this.status;
+  }
+
+  getLastUpdate(): Date {
+    return this.updatedAt;
   }
 }
