a9872b08d6dd6d326da011000315cc6c934081d2
mrdiniz88 <mr.diniz8888@gmail.com>
2023-09-08 21:56:34 -0300

    feat(transaction-service): add update method in transaction repository

--- transaction-service/src/application/repositories/TransactionRepository.ts
+++ transaction-service/src/application/repositories/TransactionRepository.ts
@@ -2,4 +2,5 @@ import Transaction from "../../domain/entities/Transaction";
 
 export default interface TransactionRepositry {
   save(transaction: Transaction): Promise<void>;
+  update(transaction: Transaction): Promise<void>;
 }
